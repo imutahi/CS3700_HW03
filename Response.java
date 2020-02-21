f436feb4f4b4a85fd7b11e9f4bf82ff1825e98c9
imutahi <imutahi@msudenver.edu>
2020-02-20 18:53:21 -0700

    Update Response.java
    Added readfile method into response.java

--- Response.java
+++ Response.java
@@ -1,13 +1,35 @@
 public class Response {
     private String filePath;
-
+    private String body; 
     public Response() {
     }
 
     public String getBody() {
-        return null;
+        return this.body;
     }
 
     public static void setFilePath(String filePath) {
+    	this.filePath = String filepath; 
+        this.body = readFile(filepath);
     }
+    
+    //Method found on Stack Overflow (author is Mr.D): https://stackoverflow.com/questions/16027229/reading-from-a-text-file-and-storing-in-a-string
+    private static String readFile(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line);
+                sb.append("\n");
+                line = br.readLine();
+            }
+            return sb.toString();
+        } finally {
+            br.close();
+        }
+    }
+
+
+
 }
